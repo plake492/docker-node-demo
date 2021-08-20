a3a325d6cfbc52a9221e6736ca19c3f73854abdc
Patrick Lake <plake492@gmail.com>
2021-08-20 17:19:11 -0400

    Update DOcker file for nodemon

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hello BOO!'))
 
 app.listen(PORT, () => console.log('app listening at http://localhost:' + PORT))
